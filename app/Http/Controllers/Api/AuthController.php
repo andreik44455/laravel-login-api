f7f039ac5e5382abc84c4b54cda6884ddfca48f4
Andrei Korovin <andrei.korovin@student.manchester.ac.uk>
2021-08-11 12:39:53 +0100

    Add login and logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,11 +5,35 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login() {
-      echo "Login endpoint requested";
+    public function login(Request $request) {
+      
+      $request->validate([
+        'email'    => 'required|string',
+        'password' => 'required|string'
+      ]);
+
+      $credentials = request(['email', 'password']);
+
+      if (!Auth::attempt($credentials)) {
+        return response()->json([
+          'message' => 'Invalid email or password'
+        ], 401);
+      }
+
+      $user = $request->user();
+
+      $token = $user->createToken('Access Token');
+
+      $user->access_token = $token->accessToken;
+
+      return response()->json([
+        "user" => $user
+      ], 200);
+
     }
 
     public function signup(Request $request) {
@@ -31,6 +55,14 @@ class AuthController extends Controller
       return response()->json([
         "message" => "User registered successfully"
       ], 201);
+    }
+
+    public function logout(Request $request) {
+      $request->user()->token()->revoke();
+
+      return response()->json([
+        "message" => "User logged out successfully"
+      ], 200);
     }
 
     public function index() {
